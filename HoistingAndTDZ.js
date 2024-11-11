406a188e1031fb26141ea0d958c2c24286c76acc
safi <safiasad12@gmail.com>
2024-11-11 20:11:00 +0530

    Hoisting & TDZ

---
+++ HoistingAndTDZ.js
@@ -0,0 +1,17 @@
+// Hoisting : printing the variable before its declaration and if the variable is declared using var is called as Hositing
+
+
+// console.log(a) //undefined
+// var a=10
+// console.log(a) // 10
+
+
+
+// TDZ: printing the variable before its declaration and if the variable is declared using let or const is called as Temoral Dead Zone
+
+
+// console.log(a)  //error (accessing before initialization)
+// let a=10 
+
+// console.log(a)  //error (accessing before initialization)
+// const a=10 
