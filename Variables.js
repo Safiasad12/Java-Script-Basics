a3d6846b45b57cc599d827bbaffdbed19d990fb8
safi <safiasad12@gmail.com>
2024-11-11 17:27:43 +0530

    variables

---
+++ Variables.js
@@ -0,0 +1,35 @@
+// variables-> these are the data holders which are used to store data. 
+// java script provides 3 keywords to store data
+// 1.var, 2.let, 3.const
+
+// var allows re-declaration as well as re-initialization
+// let only re-initialization is allowed
+// const both re-declaration and re-initialization are not allowed
+
+
+
+// var a=10
+// var a=20 // re-declaration
+// console.log(a) //20
+// a= 30 // re-initialization
+// console.log() // 30
+
+
+// let a=10
+// let a=20 // re-declaration not allowed
+// console.log(a) // error
+
+
+// let a=10
+// a=20 // re-initialization
+// console.log(a) // 20
+
+
+// const a=10
+// const a=20 // re-declaration not allowed
+// console.log(a)
+
+
+// const a=10
+// a=20 // re-initialization not allowed
+// console.log(a) // error
