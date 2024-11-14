fcbc87f03b1ef55389bab28d0b1a1dc91d7f9003
safi <safiasad12@gmail.com>
2024-11-14 22:42:49 +0530

    [safi] added: ES7 concepts

---
+++ ES7/includes.js
@@ -0,0 +1,18 @@
+// The includes() method returns true if a string contains a specified string.
+
+// Otherwise it returns false.
+
+// The includes() method is case sensitive.
+
+// string.includes(searchvalue, start)
+
+
+// *******************************
+
+
+let text = "how are you";
+
+console.log(text.includes("are")) //true
+
+console.log(text.includes("aru")) //false
+
---
+++ ES7/indexOperator.js
@@ -0,0 +1,8 @@
+// ** is a index operator which returns power of a Number
+// syntax-> base**power
+// this is equivelent to Math.pow(base, power)
+
+a=2
+console.log(2**3) // 8->2*2*2
+
+console.log(Math.pow(2, 3)) //8 
